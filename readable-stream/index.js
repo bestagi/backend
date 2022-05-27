e53471100d9ac99dc6658895f33116866508dcec
Muhamad Bestagi Romadhon <bestagi.egi@live.com>
2022-05-27 07:24:18 +0700

    Add: readable stream

---
+++ readable-stream/index.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+const readableStream = fs.createReadStream("./readable-stream/article.txt", {
+    highWaterMark: 10
+});
+
+readableStream.on("readable", () => {
+    try {
+        process.stdout.write(`[
+            ${readableStream.read()}
+        ]`);
+    } catch (error) {
+        //Menangkap error
+    }
+});
+
+readableStream.on("end", () => {
+    console.log("Done");
+});
