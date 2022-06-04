75a5a210d768b27fa2134b0631da41dc4230065f
Muhamad Bestagi Romadhon <bestagi.egi@live.com>
2022-06-05 01:44:52 +0700

    Edit: Web server req res

--- Web Server/server.js
+++ Web Server/server.js
@@ -1,1 +1,15 @@
-console.log('Halo, kita akan belajar membuat server');
+const http = require("http");
+
+const requestListener = (request, respond) => {
+    respond.setHeader("Content-Type", "text/html");
+    respond.statusCode = 200;
+    respond.end("<h1>Halo HTTP Server!</h1>");
+};
+
+const server = http.createServer(requestListener);
+const port = 5000;
+const host = "localhost";
+
+server.listen(port, host, () => {
+    console.log(`Server berjalan pada http://${host}:${port}`);
+});
