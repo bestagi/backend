c42440acf8d2e94f77e33819d18a49ddd7149a1f
Muhamad Bestagi Romadhon <bestagi.egi@live.com>
2022-06-05 02:00:38 +0700

    Edit: Handling Req

--- Web Server/server.js
+++ Web Server/server.js
@@ -3,7 +3,23 @@ const http = require("http");
 const requestListener = (request, respond) => {
     respond.setHeader("Content-Type", "text/html");
     respond.statusCode = 200;
-    respond.end("<h1>Halo HTTP Server!</h1>");
+    const {method} = request;
+
+    if(method === "GET") {
+        respond.end("<h1>Res GET!</h1>");
+    }
+
+    if(method === "POST") {
+        respond.end("<h1>Res POST!</h1>");
+    }
+
+    if(method === "PUT") {
+        respond.end("<h1>Res PUT</h1>");
+    }
+
+    if(method === "DELETE") {
+        respond.end("<h1>Res DELETE!</h1>");
+    }
 };
 
 const server = http.createServer(requestListener);
