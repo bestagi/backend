c2eeda5a5ed081e0ede5a7ca050b39eee515520a
Muhamad Bestagi Romadhon <bestagi.egi@live.com>
2022-06-05 02:31:47 +0700

    Edit: standard fix

--- Web Server/server.js
+++ Web Server/server.js
@@ -1,31 +1,31 @@
-const http = require("http");
+const http = require('http')
 
 const requestListener = (request, respond) => {
-    respond.setHeader("Content-Type", "text/html");
-    respond.statusCode = 200;
-    const {method} = request;
+  respond.setHeader('Content-Type', 'text/html')
+  respond.statusCode = 200
+  const { method } = request
 
-    if(method === "GET") {
-        respond.end("<h1>Res GET!</h1>");
-    }
+  if (method === 'GET') {
+    respond.end('<h1>Res GET!</h1>')
+  }
 
-    if(method === "POST") {
-        respond.end("<h1>Res POST!</h1>");
-    }
+  if (method === 'POST') {
+    respond.end('<h1>Res POST!</h1>')
+  }
 
-    if(method === "PUT") {
-        respond.end("<h1>Res PUT</h1>");
-    }
+  if (method === 'PUT') {
+    respond.end('<h1>Res PUT</h1>')
+  }
 
-    if(method === "DELETE") {
-        respond.end("<h1>Res DELETE!</h1>");
-    }
-};
+  if (method === 'DELETE') {
+    respond.end('<h1>Res DELETE!</h1>')
+  }
+}
 
-const server = http.createServer(requestListener);
-const port = 5000;
-const host = "localhost";
+const server = http.createServer(requestListener)
+const port = 5000
+const host = 'localhost'
 
 server.listen(port, host, () => {
-    console.log(`Server berjalan pada http://${host}:${port}`);
-});
+  console.log(`Server berjalan pada http://${host}:${port}`)
+})
